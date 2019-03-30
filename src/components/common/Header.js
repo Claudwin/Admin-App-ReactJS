285927ab5ae92c316a0f444a4deb3088bfaa58b7
Claudwin <claudwin.fortune@gmail.com>
2019-03-30 16:16:10 -0600

    Nav initial setup

---
+++ src/components/common/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  const activeStlye = { color: "#F15B2A" };
+
+  return (
+    <nav>
+      <NavLink to="/" activeStyle={activeStlye} exact>
+        {" "}
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink to="/about" activeStyle={activeStlye}>
+        About
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Header;
